84e1399c9cd2645ed0092a50753fef8624951a48
Ali Raza <alirazaabro908@gmail.com>
2022-10-09 22:39:01 +0500

    updated drawe settings with provider

--- lib/home.dart
+++ lib/home.dart
@@ -1,16 +1,16 @@
 import 'package:flutter/material.dart';
 
-class SettingsPage extends StatelessWidget {
-  const SettingsPage({Key? key}) : super(key: key);
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Setting Page"),
+        title: const Text("Home Page"),
       ),
       body: const Text(
-        "This is a setting page",
+        "This is a home page",
         style: TextStyle(fontSize: 30),
       ),
     );
